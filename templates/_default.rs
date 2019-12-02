c2894de373712b6a8484e99f6c2e2f7e9e44d980
Marcin Polak <mapoart@gmail.com>
2019-12-02 06:58:27 +0100

    Renamed the vars  eg. from NexssStdout to nexss_stdout to avoid warnings

--- templates/_default.rs
+++ templates/_default.rs
@@ -5,6 +5,7 @@
 //! serde = "*"
 //! serde_derive = "*"
 //! serde_json = "*"
+//! rustc_version = "*"
 //! ```
 
 // Nexss PROGRAMMER 2.0.0 - Rust
@@ -22,22 +23,24 @@
 extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
-
 use ::serde_derive::Serialize;
 use ::serde_derive::Deserialize;
 
-use serde_json::Value;
+extern crate rustc_version;
+use rustc_version::{version, version_meta, Channel, Version};
+
+// use serde_json::Value;
 use std::io::{self, Write};
 // use std::mem;
 fn main() -> io::Result<()> {
-    let mut NexssStdin = String::new();
+    let mut nexss_stdout = String::new();
     // let mut json = serde_json::to_string(&my_obj).expect("Couldn't serialize config");
-    match io::stdin().read_line(&mut NexssStdin) {
+    match io::stdin().read_line(&mut nexss_stdout) {
         Ok(_) => {
             // println!("{} bytes read", n);
-            let mut NexssStdout:serde_json::Value = serde_json::from_str(&NexssStdin).unwrap();
-            NexssStdout["test"] = serde_json::Value::String("test".to_string());
-            io::stdout().write_all(NexssStdout.to_string().as_bytes())?;
+            let mut nexss_stdout:serde_json::Value = serde_json::from_str(&nexss_stdout).unwrap();
+            nexss_stdout["test"] = serde_json::Value::String("test".to_string());
+            io::stdout().write_all(nexss_stdout.to_string().as_bytes())?;
         }
         Err(error) => println!("error: {}", error),
     }
--- templates/_helloWorld.rs
+++ templates/_helloWorld.rs
@@ -29,18 +29,18 @@ use ::serde_derive::Deserialize;
 extern crate rustc_version;
 use rustc_version::{version, version_meta, Channel, Version};
 
-use serde_json::Value;
+// use serde_json::Value;
 use std::io::{self, Write};
 // use std::mem;
 fn main() -> io::Result<()> {
-    let mut NexssStdin = String::new();
+    let mut nexss_stdout = String::new();
     // let mut json = serde_json::to_string(&my_obj).expect("Couldn't serialize config");
-    match io::stdin().read_line(&mut NexssStdin) {
+    match io::stdin().read_line(&mut nexss_stdout) {
         Ok(_) => {
             // println!("{} bytes read", n);
-            let mut NexssStdout:serde_json::Value = serde_json::from_str(&NexssStdin).unwrap();
-            NexssStdout["HelloFromRust"] = serde_json::Value::String(version().unwrap().to_string());
-            io::stdout().write_all(NexssStdout.to_string().as_bytes())?;
+            let mut nexss_stdout:serde_json::Value = serde_json::from_str(&nexss_stdout).unwrap();
+            nexss_stdout["HelloFromRust"] = serde_json::Value::String(version().unwrap().to_string());
+            io::stdout().write_all(nexss_stdout.to_string().as_bytes())?;
         }
         Err(error) => println!("error: {}", error),
     }
