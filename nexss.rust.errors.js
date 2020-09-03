bd62dcfd13233f6d1c62b3e62069cda9919cef68
Marcin Polak <mapoart@gmail.com>
2020-09-03 23:25:57 +0200

    Added another error definition

--- nexss.rust.errors.js
+++ nexss.rust.errors.js
@@ -3,4 +3,6 @@ module.exports = {
   "no such subcommand": `nexss rs install cargo-script`,
   "no default toolchain":
     "No default toolchain. Maybe this can fix that: rustup install stable AND rustup default stable",
+  "Permission denied":
+    "You may need to run this command with admin privilages (root?)",
 };
