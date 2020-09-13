24111dc3158bc2eacd086a9518f14dd0dc5148e2
Marcin Polak <mapoart@gmail.com>
2020-09-13 09:50:31 +0200

    Added second option for the env setup

--- nexss.rust.errors.js
+++ nexss.rust.errors.js
@@ -7,5 +7,6 @@ module.exports = {
     "You may need to run this command with admin privilages (root?)",
   "Updating crates.io":
     "At the very first run/execute it will display errors because of installing/updating creates. Try run again and there won't be any errors.",
-  "cargo: command not found": "You may need to run '. ~/bash.rc'",
+  "cargo: command not found":
+    "You may need to run '. $HOME/.cargo/env' OR '. ~/bash.rc'",
 };
